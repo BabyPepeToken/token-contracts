a0d84020285a3b222a5c27d6ccb7d04ce8d78907
Semi Invader <=>
2023-04-21 03:30:50 -0600

    complex args for verification

---
+++ complex_args.ts
@@ -0,0 +1,8 @@
+const router = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+const marketing = "0x1146e30A4d44eac8CE23730402c594Fc246e226A"
+const complex_args = [
+  [router, marketing],
+  5
+]
+
+export default complex_args;
