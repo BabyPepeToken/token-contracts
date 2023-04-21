2412b7e12b979f266b89d32b78f7152e9c8697d3
Semi Invader <=>
2023-04-21 03:00:28 -0600

    added compiler and mainnet specs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,38 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter"
+import "solidity-coverage"
+import "@nomiclabs/hardhat-etherscan"
+import { config as dotenvConfig } from "dotenv"
+dotenvConfig()
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+    },
+  },
   networks:{
     hardhat:{
       forking:{
         url: "https://eth.public-rpc.com",
         blockNumber: 17085000
       }
-    }
+    },
+    mainnet:{
+      url: "https://eth.public-rpc.com",
+      accounts: [process.env.DEPLOYER_KEY || ""]
+    },
+  },
+  gasReporter: {
+    enabled: true, // set to true of false when necessary
+  },
+  etherscan:{
+    apiKey: process.env.ETHERSCAN
   }
 };
 
